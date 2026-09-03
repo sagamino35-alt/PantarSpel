989cc557246932f7df88919077e0124fb30c77f1
SagaMino <sagaelglund@icloud.com>
2026-09-03 13:39:59 +0200

    CanvasReciver Update

--- Pant_Game/Assets/Scripts/Shared_ Other/CanvasManagerReciver_Script.cs
+++ Pant_Game/Assets/Scripts/Shared_ Other/CanvasManagerReciver_Script.cs
@@ -1,15 +1,19 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CanvasManagerReciver_Script : MonoBehaviour
 {
-    [SerializeField] CanvasManager_Script CanvasManager;
+    [SerializeField] GameObject CanvasManager;
+    [SerializeField] CanvasManager_Script CanvasManager_Script;
 
     private void Start()
     {
+        CanvasManager = GameObject.Find("CanvasManager");
+        CanvasManager_Script = CanvasManager.GetComponent<CanvasManager_Script>();
         
     }
     private void OpenP1()
     {
-
+        
     }
 }
